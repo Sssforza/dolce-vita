9171743425a0931896315749b25d32adc914c7ed
Katy <katena.moskvina@bk.ru>
2021-12-15 15:34:10 +0600

    first commit

---
+++ gulp-tasks/serve.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import { paths } from "../gulpfile.babel";
+import gulp from "gulp";
+import browsersync from "browser-sync";
+
+gulp.task("serve", () => {
+    browsersync.init({
+        port: 4000,
+        notify: true,
+        proxy: 'dolce-vita.loc'
+    });
+
+    gulp.watch(paths.styles.watch, gulp.parallel("styles"));
+    gulp.watch(paths.scripts.watch, gulp.parallel("scripts"));
+    gulp.watch(paths.modules.watch, gulp.parallel("modules"));
+    gulp.watch(paths.images.watch, gulp.parallel("images"));
+    gulp.watch(paths.images.watch, gulp.parallel("fontgen"));
+});
+
+gulp.task("serveJc", () => {
+    gulp.watch(paths.styles.watch, gulp.parallel("styles"));
+    gulp.watch(paths.scripts.watch, gulp.parallel("scripts"));
+});
