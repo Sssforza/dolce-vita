8e4c6653721e692fed47a3cb466ac516f7002871
Katy <katena.moskvina@bk.ru>
2021-12-15 15:56:31 +0600

    first files

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+//📁 /node_modules/  jquery 3.5.1
+import $ from "jquery";
+global.jQuery = global.$ = $;
+
+//📁 /node_modules/  slick 1.8.1
+import 'slick-carousel';
+
+//📁 /node_modules/  magnific-popup 1.1.0
+import 'magnific-popup';
+
+document.addEventListener("DOMContentLoaded", function () {});
