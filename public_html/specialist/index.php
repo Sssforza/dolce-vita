7a9214e2163e787290b10f279b3bd643fd4c077e
Katy <katena.moskvina@bk.ru>
2022-03-10 17:04:01 +0600

    page specialist dt

---
+++ public_html/specialist/index.php
@@ -0,0 +1,292 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+?>
+<link rel='stylesheet' href="/dist/css/blocks/specialist.css">
+<main class="main page page_specialist">
+    <section class="specialistInfo firstSection_js">
+        <div class="specialistInfo__container">
+            <div class="specialistInfo__content">
+                <section class="breadCrumbs">
+                    <ul class="breadCrumbs__list">
+                        <li class="breadCrumbs__item">
+                            <a href="/" class="breadCrumbs__link">Главная</a>
+                        </li>
+                        <li class="breadCrumbs__item">
+                            <a href="/team/" class="breadCrumbs__link">Специалисты</a>
+                        </li>
+                        <li class="breadCrumbs__item">
+                            <span class="breadCrumbs__span">Людмила Буркина</span>
+                        </li>
+                    </ul>
+                </section>
+                <div class="specialistInfo__box">
+                    <div class="specialistInfo__title">Людмила Буркина</div>
+                    <span class="specialistInfo__subtitle">Косметолог-эстетист</span>
+                    <div class="specialistInfo__description">
+                        &laquo;Глаза и&nbsp;улыбка&nbsp;&mdash; наше самое мощное оружие! С&nbsp;возрастом они неизбежно становятся не&nbsp;так выразительны, появляются морщинки, расплывается контур лица. Здорово, что мы&nbsp;умеем это корректировать и&nbsp;продлевать молодость на&nbsp;долгие годы&raquo;
+                    </div>
+                    <div class="specialistInfo__experience">
+                        <span>25 лет</span>
+                        <span>опыт работы</span>
+                    </div>
+                </div>
+            </div>
+            <div class="specialistInfo__bg">
+                <div class="specialistInfo__img" style="background-image:url(/dist/img/trash/serviceDoctor2.png)"></div>
+            </div>
+        </div>
+    </section>
+    <section class="specialistEduc specialistEduc_bg">
+        <div class="specialistEduc__content">
+            <div class="specialistEduc__title">Образование</div>
+            <div class="specialistEduc__list">
+                <div class="specialistEduc__item">
+                    <div class="specialistEduc__date">1992</div>
+                    <div class="specialistEduc__wrapper">
+                        <div class="specialistEduc__institution">Омский республиканский медицинский колледж</div>
+                        <div class="specialistEduc__specialization">
+                            <span>Специализация:</span>
+                            <span>Эстетическая косметология</span>
+                        </div>
+                    </div>
+                    <div class="specialistEduc__diploma">
+                        <a href="#" class="specialistEduc__popup">Диплом</a>
+                    </div>
+                </div>
+                <div class="specialistEduc__item">
+                    <div class="specialistEduc__date">1994</div>
+                    <div class="specialistEduc__wrapper">
+                        <div class="specialistEduc__institution">Омский республиканский медицинский колледж</div>
+                        <div class="specialistEduc__specialization">
+                            <span>Специализация:</span>
+                            <span>Лечебное дело</span>
+                        </div>
+                    </div>
+                    <div class="specialistEduc__diploma">
+                        <a href="#" class="specialistEduc__popup">Диплом</a>
+                    </div>
+                </div>
+                <div class="specialistEduc__item">
+                    <div class="specialistEduc__date">2008</div>
+                    <div class="specialistEduc__wrapper">
+                        <div class="specialistEduc__institution">Омский республиканский медицинский колледж</div>
+                        <div class="specialistEduc__specialization">
+                            <span>Специализация:</span>
+                            <span>Косметолог-эстетист с углубленной подготовкой по косметологии</span>
+                        </div>
+                    </div>
+                    <div class="specialistEduc__diploma">
+                        <a href="#" class="specialistEduc__popup">Диплом</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="specialistEduc specialistEduc_qualification">
+        <div class="specialistEduc__content">
+            <div class="specialistEduc__leaf">
+                <div class="specialistEduc__title">Повышения <br>квалификации</div>
+                <div class="specialistEduc__box">
+                    <div class="specialistEduc__date">2019</div>
+                    <div class="specialistEduc__list">
+                        <div class="specialistEduc__item">
+                            <div class="specialistEduc__specialization">
+                                <span>Cocoon Medical</span>
+                                <span>Москва</span>
+                            </div>
+                            <div class="specialistEduc__wrapper">
+                                <div class="specialistEduc__institution">
+                                    Минигрант по&nbsp;аппаратным методикам Ultherapy. Тренеры: Сайбель А.В., к.м.н., врач-дерматовенеролог,
+                                    тренер-эксперт по&nbsp;инъекционным и&nbsp;аппаратным методикам компании Merz. Бирко О.Н.,
+                                    врач-косметолог, дерматовенеролог, тренер-эксперт по&nbsp;инъекционным и&nbsp;аппаратным
+                                    методикам компании Merz
+                                </div>
+                                <div class="specialistEduc__diploma">
+                                    <a href="#" class="specialistEduc__popup">Сертификат</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="specialistEduc__item">
+                            <div class="specialistEduc__specialization">
+                                <span>Италия</span>
+                            </div>
+                            <div class="specialistEduc__wrapper">
+                                <div class="specialistEduc__institution">
+                                    Школа профессора Юцковской
+                                </div>
+                            </div>
+                        </div>
+                        <div class="specialistEduc__item">
+                            <div class="specialistEduc__specialization">
+                                <span>Омск</span>
+                            </div>
+                            <div class="specialistEduc__wrapper">
+                                <div class="specialistEduc__institution">
+                                    &laquo;Идеальные препараты для эстетического совершенства&raquo;, обучение по&nbsp;Альтера-терапии
+                                    и&nbsp;инъекционным методикам. Эксперт: Сайбель А.В., к.м.н., врач-дерматовенеролог,
+                                    тренер-эксперт по&nbsp;инъекционным и&nbsp;аппаратным методикам компании Merz
+                                </div>
+                                <div class="specialistEduc__diploma">
+                                    <a href="#" class="specialistEduc__popup">Диплом</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="specialistEduc__box">
+                    <div class="specialistEduc__date">2018</div>
+                    <div class="specialistEduc__list">
+                        <div class="specialistEduc__item">
+                            <div class="specialistEduc__specialization">
+                                <span>Москва</span>
+                            </div>
+                            <div class="specialistEduc__wrapper">
+                                <div class="specialistEduc__institution">
+                                    Школа профессора Юцковской
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="showMore">
+                <div class="showMore__btn btn btn_black">Показать еще</div>
+            </div>
+        </div>
+    </section>
+    <section class="serviceResultPhoto">
+        <div class="serviceResultPhoto__content">
+            <div class="serviceResultPhoto__header">
+                <div class="specialistEduc__title">Портфолио специалиста</div>
+                <div class="slideArrows"></div>
+            </div>
+            <div class="serviceResultPhoto__list serviceResultPhoto_js">
+                <div class="serviceResultPhoto__item">
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto1-1.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">до</div>
+                    </div>
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto1-2.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">после</div>
+                    </div>
+                </div>
+                <div class="serviceResultPhoto__item">
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto2-1.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">до</div>
+                    </div>
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto2-2.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">после</div>
+                    </div>
+                </div>
+                <div class="serviceResultPhoto__item">
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto1-1.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">до</div>
+                    </div>
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto1-2.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">после</div>
+                    </div>
+                </div>
+                <div class="serviceResultPhoto__item">
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto2-1.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">до</div>
+                    </div>
+                    <div class="serviceResultPhoto__picture">
+                        <div class="serviceResultPhoto__img" style="background-image:url(/dist/img/trash/serviceResultPhoto2-2.jpg)"></div>
+                        <div class="serviceResultPhoto__elem">после</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="serviceProcedure">
+        <div class="serviceProcedure__content">
+            <div class="serviceProcedure__header">
+                <div class="specialistEduc__title">Выполняет процедуры</div>
+                <div class="slideArrows"></div>
+            </div>
+            <div class="serviceProcedure__slider serviceProcedureSlider_js">
+                <div class="serviceProcedure__item">
+                    <a href="#" class="serviceProcedure__img serviceProcedureHover_js" style="background-image:url(/dist/img/trash/serviceProcedure1.jpg)"></a>
+                    <div class="serviceProcedure__box">
+                        <div class="serviceProcedure__name">
+                            <a href="#" class="serviceProcedure__link serviceProcedureHover_js">Альтера-терапия</a>
+                        </div>
+                        <div class="serviceProcedure__cost">от 40 000</div>
+                        <div class="serviceProcedure__seance">1 сеанс</div>
+                    </div>
+                </div>
+                <div class="serviceProcedure__item">
+                    <a href="#" class="serviceProcedure__img serviceProcedureHover_js" style="background-image:url(/dist/img/trash/serviceProcedure2.jpg)"></a>
+                    <div class="serviceProcedure__box">
+                        <div class="serviceProcedure__name">
+                            <a href="#" class="serviceProcedure__link serviceProcedureHover_js">Лазерная эпияция LightSheer Duet</a>
+                        </div>
+                        <div class="serviceProcedure__cost">7 000</div>
+                        <div class="serviceProcedure__seance">4 сеанса</div>
+                    </div>
+                </div>
+                <div class="serviceProcedure__item">
+                    <a href="#" class="serviceProcedure__img serviceProcedureHover_js" style="background-image:url(/dist/img/trash/serviceProcedure2.jpg)"></a>
+                    <div class="serviceProcedure__box">
+                        <div class="serviceProcedure__name">
+                            <a href="#" class="serviceProcedure__link serviceProcedureHover_js">Криолиполиз</a>
+                        </div>
+                        <div class="serviceProcedure__cost">от 15 500</div>
+                        <div class="serviceProcedure__seance">от 3 сеансов</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="specialistRecord">
+        <div class="specialistRecord__content">
+            <div class="signConsultation specialistRecord__form">
+                <form class="signConsultation__form signConsultation_js">
+                    <div class="signConsultation__title">Запишитесь <br>на консультацию</div>
+                    <div class="signConsultation__fileds">
+                        <div class="envelopeInput">
+                            <input type="text" placeholder="Ваше имя" class="name fileds_js">
+                        </div>
+                        <div class="envelopeInput">
+                            <input type="text" placeholder="Телефон" class="tel">
+                            <span class="envelopeInput__warning">Некорректный формат телефона</span>
+                        </div>
+                    </div>
+                    <div class="signConsultation__bottom">
+                        <div class="signConsultation__wrapper">
+                            <button class="btn btn_black signConsultation__btn">Записаться</button>
+                            <div class="signConsultation__privacy">Нажимая на&nbsp;кнопку, я&nbsp;соглашаюсь на&nbsp;обработку
+                                <a href="#">персональных данных</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="signConsultation__message signMessage signMessage_error signMessageError_js">
+                    <div class="signMessage__content">
+                        <div class="signMessage__title">Произошла <br>ошибка</div>
+                        <div class="signMessage__text">Пожалуйста попробуйте отправить форму еще раз</div>
+                        <div class="btn btn_black signMessage__btn signMessageBtn_js">Назад</div>
+                    </div>
+                </div>
+                <div class="signConsultation__message signMessage signMessageAccess_js">
+                    <div class="signMessage__content">
+                        <div class="signMessage__title">Спасибо <br>за&nbsp;обращение</div>
+                        <div class="signMessage__text">Менеджер свяжется с&nbsp;вами в&nbsp;ближайшее время</div>
+                    </div>
+                </div>
+            </div>
+            <div class="specialistRecord__img" style="background-image:url(/dist/img/trash/serviceDoctor2.png)"></div>
+            </div>
+        </div>
+    </div>
+</main>
+<div class="smallHeader_js hidden"></div>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
